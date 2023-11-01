15552cc11b35e070383b605a53b8295299a28e77
Akash Das <acd4007@med.cornell.edu>
2023-11-01 17:05:30 -0400

    new commit

--- R/Function_1.R
+++ R/Function_1.R
@@ -1,57 +1,56 @@
+library(tidyverse)
 #' Create a Boxplot
 #'
 #' This function creates a boxplot for DRG data by taking input of type of payment
 #'
-#' @param payment Type of payment
+#' @param payment Type of payment - "medicare", "total", "covered"
 #' @return Boxplot
 #' @export
 payment <- function(payment_type)
 {
-  # Group by each of the three categories
+  data <- read_csv("data/DRG_data.csv")
+
   medicare <- data %>%
     group_by(DRG.Definition) %>%
     summarise(mean = mean(Average.Medicare.Payments))
-  total_payment <- data %>%
+  total <- data %>%
     group_by(DRG.Definition) %>%
     summarise(mean = mean(Average.Total.Payments))
-  covered_charges <- data %>%
+  covered <- data %>%
     group_by(DRG.Definition) %>%
     summarise(mean = mean(Average.Covered.Charges))
-  
-  
-  # Check which category the user wants to plot
+
   if (payment_type == "medicare"){
-    
-    # Plot it
-    p <- ggplot(medicare,
-                aes(x=DRG.Definition, y=mean)) +
+    plt <- medicare %>%
+      ggplot(aes(x=DRG.Definition,
+                 y=mean)) +
       geom_boxplot() +
-      xlab("DRG Codes") +
-      ylab("Average Medicare Payment (in dollars)") +
-      ggtitle("Average Medicare Payment by DRG Code") +
-      theme(plot.title = element_text(hjust = 0.5))
-    print(p)
+      labs(x = "DRG Codes",
+           y = "Average Medicare Payment (dollars)",
+           title = "Average Medicare Payment by DRG Code") +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      theme_minimal()
   }
-  else if (payment_type == "total_payment"){
-    p <- ggplot(total_payment,
-                aes(x=DRG.Definition, y=mean)) +
+  else if (payment_type == "total"){
+    plt <- total %>%
+      ggplot(aes(x=DRG.Definition,
+                 y=mean)) +
       geom_boxplot() +
-      xlab("DRG Codes") +
-      ylab("Average Total Payment (in dollars)") +
-      ggtitle("Average Total Payment by DRG Code") +
-      theme(plot.title = element_text(hjust = 0.5))
-    print(p)
+      labs(x = "DRG Codes",
+           y = "Average Total Payment (dollars)",
+           title = "Average Total Payment by DRG Code") +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      theme_minimal()
   }
   else{
-    p <- ggplot(covered_charges,
-                aes(x=DRG.Definition, y=mean)) +
+    plt <- covered %>%
+      ggplot(aes(x=DRG.Definition,
+                 y=mean)) +
       geom_boxplot() +
-      xlab("DRG Codes") +
-      ylab("Average Covered Charges (in dollars)") +
-      ggtitle("Average Covered Charges by DRG Code") +
-      theme(plot.title = element_text(hjust = 0.5))
-    print(p)
+      labs(x = "DRG Codes",
+           y = "Average Covered Payment (dollars)",
+           title = "Average Covered Payment by DRG Code") +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      theme_minimal()
   }
-  
-  
 }
--- R/Function_2.R
+++ R/Function_2.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 #' Function calculates either the mean, median, or standard deviation of the DRG
 #' codes. over all of the DRG codes for average Medicare payments
 #'
@@ -7,25 +8,5 @@
 #' @return returns the chosen statistic calculated
 #' @export
 stats <- function(statistics){
-  # Assign different number to indicate the location of the statistics
-  if(statistics == "mean"){
-    i <- 2
-  }else if(statistics == "medium"){
-    i <- 3
-  }else{
-    i <- 4
-  }
-  da1 <- data %>%
-    # Calculate the mean, medium, and standard deviation of each group
-    group_by(DRG.Definition) %>%
-    summarise(Mean = mean(Average.Medicare.Payments),
-              Medium = median(Average.Medicare.Payments),
-              Standard_Deviation  = sd(Average.Medicare.Payments))
-  #  Replace (.) with spaces
-  colnames(da1) <- gsub("\\.", " ", colnames(da1))
-  #  Replace (_) with spaces
-  colnames(da1) <- gsub("\\_", " ", colnames(da1))
-  # return the statistics that we choose
-  output2 <- knitr::kable(da1[,c(1,i)])
-  return(output2)
+  ## PASTE YOUR FUNCTION HERE
 }
