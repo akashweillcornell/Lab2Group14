9e71f5cee860a3744b43d5a5d91adadeaa9315e0
Akash Das <acd4007@med.cornell.edu>
2023-11-01 17:23:41 -0400

    Modified Xiao's code to fit

--- R/Function_2.R
+++ R/Function_2.R
@@ -7,11 +7,10 @@ library(tidyverse)
 #'
 #' @return returns the chosen statistic calculated
 #' @export
-#' @importFrom dplyr summarise
-#' @importFrom dplyr pull
-#' @importFrom dplyr %>%
-#' @export
-stats <- function(data, stat_type = "mean") {
+stats <- function(stat_type = "mean") {
+
+  data <- read.csv("data/DRG_data.csv")
+
   # Check if the input statistic is valid
   if (!(stat_type %in% c("mean", "median", "sd"))) {
     stop("Invalid statistic. Please choose 'mean', 'median', or 'sd'.")
@@ -27,7 +26,7 @@ stats <- function(data, stat_type = "mean") {
   # Get statistics based on the  input stat_type
   result <- data %>%
     summarise(
-      stat_value = calculate[[stat_type]](`Average Medicare Payments`)
+      stat_value = calculate[[stat_type]](Average.Medicare.Payments)
     ) %>%
     pull(stat_value)
 
